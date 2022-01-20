5a1875117e0ae664fb9684162c44b70a7e4b8f0f
Catherine Justine Zuniga <qcjdzuniga@tip.edu.ph>
2022-01-20 16:46:45 +0800

    added function TaskList

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function TaskList(){
+    return(
+        <ul>
+            <li>Follow Edukasyon.ph on FB</li>
+            <li>Follow AWS Siklab Pilipinas on FB</li>
+            <li>Follow Zuitt Coding Bootcamp</li>
+            <li>ฅ^•ﻌ•^ฅ</li>
+        </ul>
+    );
+}
+
 function App(){
 
     const paraStyle = {
@@ -13,12 +24,7 @@ function App(){
             <header className="app-header">
                 <h1>To Do List App</h1>
                 <p style={paraStyle}>A simple yet amazing To Do List React App.</p>
-                <ul>
-                    <li>Follow Edukasyon.ph on FB</li>
-                    <li>Follow AWS Siklab Pilipinas on FB</li>
-                    <li>Follow Zuitt Coding Bootcamp</li>
-                    <li>ฅ^•ﻌ•^ฅ</li>
-                </ul>
+                <TaskList/>
             </header>
         </div>
     );
