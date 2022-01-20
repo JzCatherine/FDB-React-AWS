91a72c44e442b538f0a958c054fa0453f3b5552f
Catherine Justine Zuniga <qcjdzuniga@tip.edu.ph>
2022-01-20 16:31:34 +0800

    list external style & inline style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App(){
+
+    const paraStyle = {
+        "marginBottom": "15px"
+    };
+
     return(
         <div className="App-logo">
             <header className="app-header">
                 <h1>To Do List App</h1>
-                <p>A simple yet amazing To Do List React App.</p>
+                <p style={paraStyle}>A simple yet amazing To Do List React App.</p>
+                <ul>
+                    <li>Follow Edukasyon.ph on FB</li>
+                    <li>Follow AWS Siklab Pilipinas on FB</li>
+                    <li>Follow Zuitt Coding Bootcamp</li>
+                    <li>ฅ^•ﻌ•^ฅ</li>
+                </ul>
             </header>
         </div>
     );
